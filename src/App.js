daf16cb7c9c43f39641918744fc8293538d211f2
muntasir-smm <munna112ru@gmail.com>
2024-06-06 22:40:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const singer = { name: "Dr Mahfuz", job: "Singer" };
+const singer2 = { name: "Runa Khan", job: "Singer2" };
+const singerStyle = {
+  color: "Purple",
+  backgroundColor: "white",
+  padding: "10px",
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>JSX</h2>
+        <div className="container">
+          <h3>Hello Dude!! How are you?</h3>
+        </div>
+
+        <div className="music">
+          <p style={singerStyle}>
+            Name: {singer.name} <br />
+            Job: {singer.job}
+          </p>
+          <p
+            style={{
+              color: "blue",
+              backgroundColor: "yellow",
+              padding: "20px",
+            }}
+          >
+            Name: {singer2.name} <br />
+            Job: {singer2.job}
+          </p>
+        </div>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
